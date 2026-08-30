c3c316ac85d9f9c30cad8e280a9919e2c821a904
cryphon <yvanroes@gmail.com>
2026-08-30 23:56:34 +0200

    feat: add SLIP encode and decode definitions

--- include/serial.h
+++ include/serial.h
@@ -5,4 +5,7 @@
 int serial_open(const char* device, speed_t baud); // returns fd or -1
 int serial_close(int fd);
 
+int serial_send_byte(int fd, unsigned char c);
+int serial_recv_byte(int fd, unsigned char* out);
+
 #endif
